d2db988207bd4742f48a0bee2912fdb7c9d42c60
shubhu <shubhu@ev>
2022-09-28 12:16:30 +0530

    UI Update2

---
+++ Second.cpp
@@ -0,0 +1,6 @@
+#include<iostream>
+using namespace std;
+int main(){
+cout<<"NAMASTE C++ !!!"<<endl;
+return 0;
+}
---
+++ third.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+cout<<"How are you"<<endl;
+return 0;
+}
